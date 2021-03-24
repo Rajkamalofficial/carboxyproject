522c4f98947f91dd4e0189b73525b49cf177a205
Rajkamalofficial <rajflux@gmail.com>
2021-03-24 10:58:02 +0530

    fix sidebar

--- carboxy_app/src/SidebarOption.js
+++ carboxy_app/src/SidebarOption.js
@@ -4,8 +4,10 @@ import "./SidebarOption.css";
 function SidebarOption({ active, text, Icon }) {
   return (
     <div className={`sidebarOption ${active && "sidebarOption--active"}`}>
+      <div class="sidebaroptioncontainer">
       <Icon />
       <h2>{text}</h2>
+      </div>
     </div>
   );
 }
